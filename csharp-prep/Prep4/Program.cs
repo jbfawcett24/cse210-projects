42f4bd662afcc6772de6f0d756408df4f0c90d88
jbfawcett24 <jbfawcett24@gmail.com>
2024-10-31 11:00:47 -0600

    started prep 4

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -1,9 +1,16 @@
 using System;
+using System.Reflection.Metadata;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+        string input;
+        int intInput = -1;
+        List<int> numbers = new List<int>();
+        Console.WriteLine("enter a list of numbers, type 0 to stop.");
+        while(intInput != 0){
+            Console.Write("Enter number: ");
+        }
     }
 }
