8ef08d2522af47b115b8b61ed9d8dd006980cd15
jbfawcett24 <jbfawcett24@gmail.com>
2024-11-04 09:58:56 -0700

    little fixes to prep5

--- csharp-prep/Prep5/Program.cs
+++ csharp-prep/Prep5/Program.cs
@@ -3,6 +3,34 @@ using System.Globalization;
 
 class Program
 {
+static void DisplayWelcome()
+{
+    Console.WriteLine("Welcome to the Program!");
+}
+static string PromptUserName()
+{
+    string name;
+    Console.Write("Enter your name: ");
+    name = Console.ReadLine();
+    return name;
+}
+static string PromptUserNumber()
+{
+    string number;
+    Console.Write("Enter your favorite number: ");
+    number = Console.ReadLine();
+    return number;
+}
+static int SquareNumber(string number)
+{
+    int intNum = int.Parse(number);
+    intNum *= intNum;
+    return intNum;
+}
+static void DisplayResult(string name, int number)
+{
+    Console.WriteLine($"{name}, the square of your number is {number}");
+}    
     static void Main(string[] args)
     {
         DisplayWelcome();
@@ -11,37 +39,6 @@ class Program
         int squareNum = SquareNumber(userNumber);
         DisplayResult(userName, squareNum);
 
-        static void DisplayWelcome()
-        {
-            Console.WriteLine("Welcome to the Program!");
-        }
 
-        static string PromptUserName()
-        {
-            string name;
-            Console.Write("Enter your name: ");
-            name = Console.ReadLine();
-            return name;
-        }
-
-        static string PromptUserNumber()
-        {
-            string number;
-            Console.Write("Enter your favorite number: ");
-            number = Console.ReadLine();
-            return number;
-        }
-
-        static int SquareNumber(string number)
-        {
-            int intNum = int.Parse(number);
-            intNum *= intNum;
-            return intNum;
-        }
-
-        static void DisplayResult(string name, int number)
-        {
-            Console.WriteLine($"{name}, the square of your number is {number}");
-        }
     }
 }
