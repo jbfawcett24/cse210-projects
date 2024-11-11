8874119e6bdec20f0a07afb9fa815de14524a0b2
jbfawcett24 <jbfawcett24@gmail.com>
2024-11-11 09:56:58 -0700

    changes to prep 2

--- prepare/Learning02/Job.cs
+++ prepare/Learning02/Job.cs
@@ -1,11 +1,19 @@
 
+using System.Security.AccessControl;
+
 public class Job
 {
-    public string _company;
-    public string _jobTitle;
-    public int _startYear;
-    public int _endYear;
-
+    private string _company;
+    private string _jobTitle;
+    private int _startYear;
+    private int _endYear;
+    public Job(string company, string jobTitle, int startYear, int endYear)
+    {
+        _company = company;
+        _jobTitle = jobTitle;
+        _startYear = startYear;
+        _endYear = endYear;
+    }
     public void DisplayJobDetails()
     {
         Console.WriteLine($"{_jobTitle} ({_company}) {_startYear}-{_endYear}");
--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -5,19 +5,9 @@ class Program
 {
     static void Main(string[] args)
     {
-        Job job1 = new Job();
-        Job job2 = new Job();
+        Job job1 = new Job("Software Engineer", "Microsoft", 2019, 2022);
+        Job job2 = new Job("Manager", "Apple", 2022, 2023);
         Resume resume = new Resume();
-
-        job1._jobTitle = "Software Engineer";
-        job1._company = "Microsoft";
-        job1._startYear = 2019;
-        job1._endYear = 2022;
-
-        job2._jobTitle = "Manager";
-        job2._company = "Apple";
-        job2._startYear = 2022;
-        job2._endYear = 2023;
 
         resume._name = "James";
         resume._jobs.Add(job1);
